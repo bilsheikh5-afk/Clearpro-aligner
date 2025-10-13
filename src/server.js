7a254b3cb387e7a46dae10b58e67400dd9d26011
bilsheikh5-afk <bilsheikh5@gmail.com>
2025-10-13 11:04:41 +0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import app from './app.js';
-const PORT = process.env.PORT || 4000;
+
+const PORT = process.env.PORT || 10000; // ✅ changed default to 10000
+
 app.listen(PORT, () => {
-  console.log(`API running on port ${PORT}`);
+  console.log(`✅ API running on port ${PORT}`);
 });
